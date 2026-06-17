402ed8cf4a8109b85a52604ce4a270d3673188ee
Ifforudba <usamahalqorni@gmail.com>
2026-06-17 11:16:33 +0700

    first update

---
+++ javatutorial/src/main/java/com/mycompany/javatutorial/Javatutorial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javatutorial;
+
+/**
+ *
+ * @author VIVOBOOK
+ */
+public class Javatutorial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
